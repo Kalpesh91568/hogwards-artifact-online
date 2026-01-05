93ec92d56af72a165bf1dd604628e50a7af0f044
jatin-ukey21 <jatinukey1@gmail.com>
2026-01-05 16:33:37 +0530

    Project initialization

---
+++ src/main/java/com/spring/hogwards/hogwards_artifact_online/HogwardsArtifactOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.spring.hogwards.hogwards_artifact_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwardsArtifactOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwardsArtifactOnlineApplication.class, args);
+	}
+
+}
